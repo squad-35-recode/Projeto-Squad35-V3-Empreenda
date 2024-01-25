1335c1862ec8cf029ee0689db90659d2e1834c0a
Manoel Monteiro <manoel.monteiro1@outlook.com>
2024-01-24 21:49:04 -0300

    Revert "update"
    This reverts commit 5662076a0d883395d50e8a10c03035fc93ddfd91.

--- src/main/java/com/example/springbootjava/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootjava.repository;
-
-import com.example.springbootjava.domain.Todo;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface TodoRepository extends CrudRepository<Todo, Long> { }
